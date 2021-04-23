eac74c86d952a85281c06a80ddc934de5ac450f2
HuynhThiMyDuyen <19521438@gmail.com>
2021-04-23 14:09:32 +0700

    update N-Queens.py

--- Homework/Group-03/N-Queens.py
+++ Homework/Group-03/N-Queens.py
@@ -1,14 +1,12 @@
 result = []
 
  
-def isSafe(board, row, col):
+def isSafe(board, row, col, n):
  
-    # Check this row on left side
     for i in range(col):
         if (board[row][i]):
             return False
  
-    # Check upper diagonal on left side
     i = row
     j = col
     while i >= 0 and j >= 0:
@@ -17,10 +15,9 @@ def isSafe(board, row, col):
         i -= 1
         j -= 1
  
-    # Check lower diagonal on left side
     i = row
     j = col
-    while j >= 0 and i < 4:
+    while j >= 0 and i < n:
         if(board[i][j]):
             return False
         i = i + 1
@@ -34,25 +31,22 @@ def solveNQUtil(board, col, n):
     if (col == n):
         v = []
         for i in board:
-          for j in range(len(i)):
-            if i[j] == 1:
-              v.append(j+1)
+            for j in range(len(i)):
+                if i[j] == 1:
+                    v.append(j+1)
         result.append(v)
         return True
 
     res = False
     for i in range(n):
 
-        if (isSafe(board, i, col)):
+        if (isSafe(board, i, col, n)):
  
-            # Place this queen in board[i][col]
             board[i][col] = 1
  
-            # Make result true if any placement
-            # is possible
             res = solveNQUtil(board, col + 1, n) or res
  
-            board[i][col] = 0  # BACKTRACK
+            board[i][col] = 0 
  
     return res
  
@@ -66,9 +60,7 @@ def solveNQ(n):
     result.sort()
     return result
  
- 
-# Driver Code
-n = 10
+
+n = int(input())
 res = solveNQ(n)
 print(len(res))
-# This code is contributed by YatinGupta
